1be07cd7f0c2bdad75ea70e9daec11c5d2b76f7c
Exp <17885980+exp111@users.noreply.github.com>
2024-12-10 13:17:21 +0100

    cytoscape

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Power} from '../../model/power';
 import {Powers} from '../../data/powers';
+import cytoscape from 'cytoscape';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,13 @@ import {Powers} from '../../data/powers';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'PathOfAchraGraph';
   powers = new Array<Power|null>(8).fill(null);
+  cytoscape!: cytoscape.Core;
+
+  @ViewChild("cytoscape")
+  cytoscapeContainer!: ElementRef;
 
   constructor() {
     this.powers[0] = Powers.find(p => p.name == "Toxokinesis")!;
@@ -25,5 +30,22 @@ export class AppComponent {
     //TODO: check for duplicates
     let i = this.powers.findIndex(p => !p);
     this.powers[i] = Powers[Math.floor(Math.random() * Powers.length)];
+  }
+
+  ngAfterViewInit(): void {
+    this.cytoscape = cytoscape({
+      container: this.cytoscapeContainer.nativeElement, // container to render in
+      style: [
+        {
+          selector: 'node',
+          style: {
+            'background-color': '#666',
+            'label': 'data(id)'
+          }
+        }
+      ]
+    });
+    let a = this.cytoscape.add({data: {id: "a"}});
+    this.cytoscape.center(a);
   }
 }
