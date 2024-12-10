d2aba37b1f005c8ef12741b7e438ef669852d5b8
Exp <17885980+exp111@users.noreply.github.com>
2024-12-10 13:35:36 +0100

    dont add duplicate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,16 +22,6 @@ export class AppComponent implements AfterViewInit {
     this.powers[0] = Powers.find(p => p.name == "Toxokinesis")!;
   }
 
-  powerClicked(index: number) {
-    this.powers[index] = null!;
-  }
-
-  addRandomPower() {
-    //TODO: check for duplicates
-    let i = this.powers.findIndex(p => !p);
-    this.powers[i] = Powers[Math.floor(Math.random() * Powers.length)];
-  }
-
   ngAfterViewInit(): void {
     this.cytoscape = cytoscape({
       container: this.cytoscapeContainer.nativeElement, // container to render in
@@ -47,5 +37,24 @@ export class AppComponent implements AfterViewInit {
     });
     let a = this.cytoscape.add({data: {id: "a"}});
     this.cytoscape.center(a);
+  }
+
+  powerClicked(index: number) {
+    this.powers[index] = null!;
+  }
+
+  addRandomPower() {
+    let i = this.powers.findIndex(p => !p);
+    // dont add a duplicate one
+    let filtered = Powers.filter(p => !this.powers.some(po => po?.name == p.name));
+    if (filtered.length == 0) {
+      console.log("No more powers.");
+      return;
+    }
+    this.powers[i] = filtered[Math.floor(Math.random() * filtered.length)];
+  }
+
+  buildGraph() {
+
   }
 }
